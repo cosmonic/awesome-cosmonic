d61838cf2f02ac8b3b0f7b2066a2947ce4e1eb80
Connor Smith <connor.smith.256@gmail.com>
2023-05-02 11:59:41 -0600

    update message to point to walkthrough

--- hello-world/rust/src/lib.rs
+++ hello-world/rust/src/lib.rs
@@ -9,7 +9,15 @@ struct {{to_pascal_case project-name}}Actor {}
 #[async_trait]
 impl HttpServer for {{to_pascal_case project-name}}Actor {
     async fn handle_request(&self, _ctx: &Context, _req: &HttpRequest) -> RpcResult<HttpResponse> {
-        HttpResponse::ok("Hello, World!")
+        let message: &str = r#"Hello, World, from Cosmonic!
+
+Congratulations on deploying this actor! Wondering what to build next?
+Check out our Things to Build guide on an XKCD image generator:
+
+https://cosmonic.com/docs/to-build/image-generator
+"#;
+
+        HttpResponse::ok(message)
     }
 }
 
