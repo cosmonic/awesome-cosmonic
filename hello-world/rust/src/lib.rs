2bb1bfd4aa48ea108d92c548807561c3b5717a8c
aish-where-ya <aharpale@cosmonic.com>
2023-08-25 17:40:51 -0400

    Bug fixes and using wash for KVCounter
    Signed-off-by: aish-where-ya <aharpale@cosmonic.com>

--- hello-world/rust/src/lib.rs
+++ hello-world/rust/src/lib.rs
@@ -17,7 +17,7 @@ Check out our Things to Build guide on an XKCD image generator:
 https://cosmonic.com/docs/to-build/image-generator
 "#;
 
-        HttpResponse::ok(message)
+        Ok(HttpResponse::ok(message))
     }
 }
 
